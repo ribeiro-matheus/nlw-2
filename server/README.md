5ed8c9dd9697f9233e874f0ff215744e74db9e7f
ribeiro-matheus <matheusribeiro.github@gmail.com>
2020-08-10 16:50:35 -0300

    db users

---
+++ server/README.md
@@ -0,0 +1,11 @@
+# funcionalidades
+
+## conexões
+
+-rota para listar o total de conexões realizadas;
+-rota para criar uma nova conexão;
+
+# aulas
+-rota  para criar uma aula;
+-rota para listar aulas;
+ -filtrar por materia, dia da semana, horario
---
+++ server/dicas.js
@@ -0,0 +1,9 @@
+//GET: buscar ou listar uma informação
+//POST:criar alguma nova informação
+//PUT:atualizar uma informação existente
+//DELETE:deletar uma informação existente
+
+//parametros
+//corpo(request body): dados para criação oun atualização de um registro
+//route params: identificar qual recurso eu quro atualizar ou deletar
+//query params: paginação, filtros, ordenação 
